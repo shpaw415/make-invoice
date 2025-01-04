3e80520c9d9148d9ba9322e5f0be68efccaa13e9
justin halle <shpaw415@gmail.com>
2025-01-04 13:12:19 -0500

    pre release

--- index.ts
+++ index.ts
@@ -1,19 +1,25 @@
 import { renderToString } from "react-dom/server";
 import { launch } from "puppeteer";
 import Template, { type TemplateData } from "./template/template";
-import { readFileSync } from "node:fs"
+import { readFileSync } from "node:fs";
+import type { PuppeteerLaunchOptions } from "puppeteer";
+
 class Invoice {
-    pdf: Uint8Array<ArrayBufferLike> | undefined;
-    async make(data: TemplateData) {
-        const browser = await launch();
-        const page = await browser.newPage();
-        await page.setContent(renderToString(Template(data)));
-        await page.addStyleTag({ content: readFileSync(`${import.meta.dirname}/template/style.css`).toString("utf-8") })
-        const pdf = await page.pdf({ format: "A4" });
-        page.close();
-        browser.close();
-        return pdf;
-    }
+  pdf: Uint8Array<ArrayBufferLike> | undefined;
+  async make(data: TemplateData, options?: PuppeteerLaunchOptions) {
+    const browser = await launch(options);
+    const page = await browser.newPage();
+    await page.setContent(renderToString(Template(data)));
+    await page.addStyleTag({
+      content: readFileSync(
+        `${import.meta.dirname}/template/style.css`
+      ).toString("utf-8"),
+    });
+    const pdf = await page.pdf({ format: "A4" });
+    page.close();
+    browser.close();
+    return pdf;
+  }
 }
 
 export { Invoice };
